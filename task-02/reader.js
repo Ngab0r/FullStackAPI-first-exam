a25b2daec8082fb799cc787fef58f77f08a6519c
Nemcsics Gábor <nemcsics.gabor@gmail.com>
2021-07-05 15:35:52 +0200

    feladat 2 donefeladat 3 done

--- task-02/reader.js
+++ task-02/reader.js
@@ -6,7 +6,11 @@
  * 2. Állítsd be az azonos mappában található .json fájl elérési útját a path 
  * modul join metódusának segítségével.
  */
-const jsonPath = '';
+const fsp = require('fs').promises
+const { join } = require('path')
+
+
+const jsonPath = join(__dirname, 'db', 'users.json')
 
 /**
  * 3. A jsonPath útvonalon található fájl tartalmát beolvassa és értelmezi, 
@@ -14,9 +18,10 @@ const jsonPath = '';
  * 
  * @returns objektumok tömbje
  */
- const getList = async () => {
-     //
-};
+const getList = async () => {
+    const content = await fsp.readFile(jsonPath, 'utf8')
+    return JSON.parse(content)
+}
 
 /**
  * 4. A db/users.json fájlban tárolt 'users' tömbből egy elem kiválasztása az id 
@@ -27,9 +32,10 @@ const jsonPath = '';
  * @returns a tömbnek az az eleme, amelynek az id -je megegyezik a kapottal
  */
 const get = async (id = 0) => {
-    //
-};
+    const list = await getList()
+    return list.find(item => item.id === id)
+}
 
 module.exports = {
-    get,
-};
+    get
+}
